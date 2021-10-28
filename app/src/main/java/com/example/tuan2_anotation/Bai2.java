9e2cdd9291a63ccde0fe139a47ebe2952cf8baed
nguyen234-p <80658161+nguyen234-p@users.noreply.github.com>
2021-10-28 15:19:15 +0700

    anotation

--- app/src/main/java/com/example/tuan2_anotation/Bai2.java
+++ app/src/main/java/com/example/tuan2_anotation/Bai2.java
@@ -3,12 +3,17 @@ package com.example.tuan2_anotation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Bai2 extends AppCompatActivity {
-
+    ImageView img1,img2,img3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bai2);
+
+        img1=findViewById(R.id.imageView);
+        img1=findViewById(R.id.imageView10);
+        img1=findViewById(R.id.imageView11);
     }
 }
--- app/src/main/java/com/example/tuan2_anotation/MainActivity2.java
+++ app/src/main/java/com/example/tuan2_anotation/MainActivity2.java
@@ -3,12 +3,16 @@ package com.example.tuan2_anotation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    ImageView img1, img2,img3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        img1 = findViewById(R.id.imageView4);
+        img1 = findViewById(R.id.imageView7);
+        img1 = findViewById(R.id.imageView9);
     }
 }
